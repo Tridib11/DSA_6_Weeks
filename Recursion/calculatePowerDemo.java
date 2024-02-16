a3f5eb0b0b342ad0aebe7325410e404823362c74
Tridib11 <tridibghosh12345@gmail.com>
2024-02-16 23:30:13 +0530

    Files Modified

--- Recursion/calculatePowerDemo.java
+++ Recursion/calculatePowerDemo.java
@@ -1,15 +1,35 @@
 public class calculatePowerDemo {
     public static void main(String[] args) {
+
+        //1st Function
+
         long startTime=System.nanoTime();
         System.out.println(calculatePowerDemo1(2, 10));
         long endTime=System.nanoTime();
-        double durationInNano=endTime-startTime;
-        double durationInMillis = durationInNano / 1_000_000.0;
-        System.out.println(durationInMillis);
+        System.out.println((endTime-startTime)/1_000_000.0 +" sec");
+
+
+        long startTime2=System.nanoTime();
+        System.out.println(calculatePowerDemo2(2, 10));
+        long endTime2=System.nanoTime();
+        System.out.println((endTime2-startTime2)/1_000_000.0 +" sec");
+
+
     }
 
     public static int calculatePowerDemo1(int x, int n) {
         if (n == 0) return 1;
         return x * calculatePowerDemo1(x, n - 1);
+    }
+
+    public static int calculatePowerDemo2(int x,int n){
+        if(n==0) return 1;
+        if(n%2==0){
+            int temp=calculatePowerDemo2(x, n/2);
+            return temp*temp;
+        }
+        else{
+            return calculatePowerDemo2(x, n-1)*x;
+        }
     }
 }
