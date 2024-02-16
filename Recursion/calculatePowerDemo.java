c7dde50312608e98f8fd104c81282a6e45c36646
Tridib11 <tridibghosh12345@gmail.com>
2024-02-16 22:41:23 +0530

    Files modiefied

--- Recursion/calculatePowerDemo.java
+++ Recursion/calculatePowerDemo.java
@@ -1,11 +1,10 @@
 public class calculatePowerDemo {
     public static void main(String[] args) {
-        int c=calculatePowerDemo1(2, 100);
-        System.out.println(c);
+        System.out.println(calculatePowerDemo1(2, 10));
     }
 
-    static int calculatePowerDemo1(int x,int n){
-        if(n==0) return 1;
-        return x*calculatePowerDemo1(x, n-1);
+    public static int calculatePowerDemo1(int x, int n) {
+        if (n == 0) return 1;
+        return x * calculatePowerDemo1(x, n - 1);
     }
 }
