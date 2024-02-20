ca7c10f6256e3d91ce33d3d7a8447ead6387d11f
Tridib11 <tridibghosh12345@gmail.com>
2024-02-20 10:38:41 +0530

    File modified

--- Recursion/Subsets.java
+++ Recursion/Subsets.java
@@ -1,28 +1,27 @@
 // https://leetcode.com/problems/subsets
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
-       List<Integer> curr=new ArrayList<>();
-       List<List<Integer>> res=new ArrayList<>();
-       helper(0,nums,curr,res); 
-       return res;
+        List<Integer> curr = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        helper(0, nums, curr, res);
+        return res;
     }
 
-    public void helper(int idx,int[] nums,List<Integer> curr,List<List<Integer>> res){
-        if(idx==nums.length){
+    public void helper(int idx, int[] nums, List<Integer> curr, List<List<Integer>> res) {
+        if (idx == nums.length) {
             res.add(new ArrayList<>(curr));
             return;
         }
-        //include
+        // include
         curr.add(nums[idx]);
-        helper(idx+1,nums,curr,res);
+        helper(idx + 1, nums, curr, res);
         curr.remove(curr.size() - 1);
-        //exclude
-        helper(idx+1,nums,curr,res);
+        // exclude
+        helper(idx + 1, nums, curr, res);
 
     }
 }
