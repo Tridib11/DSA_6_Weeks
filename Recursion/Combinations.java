83f43281ccc68a320866013ee62b2ae60f7a7888
Tridib11 <tridibghosh12345@gmail.com>
2024-02-23 21:37:17 +0530

    Files modified

---
+++ Recursion/Combinations.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Combinations {
+    
+}
+
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> subsets=new ArrayList<>();
+        generateSubsets(1,n,new ArrayList<>(),subsets,k);
+        return subsets;     
+    }
+    static void  generateSubsets(int start,int n,List<Integer> current,List<List<Integer>> subsets,int k){
+        if(current.size()==k){
+            subsets.add(new ArrayList<>(current));
+            return;
+        }
+        for(int i=start;i<=n;i++){
+            current.add(i);
+            generateSubsets(i+1,n,current,subsets,k);
+            current.removeLast();
+        }
+    }
+
+}
